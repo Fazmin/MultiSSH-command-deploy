505cd11026ba74c2e4123d0a3362f14533e7e8c9
Fazmin Nizam <fazmin.nizam@gmail.com>
2023-03-18 12:40:47 -0400

    Update sshConnectRun.py

--- sshConnectRun.py
+++ sshConnectRun.py
@@ -1,9 +1,10 @@
-# Submit commands and capture output on a multi ssh connections using just python.  
+# Submit commands and capture output on a multi ssh connections using just python.
+# Version 1.1 - Tested - Python 3.8 - Fazmin Nizam
 # make sure you have paramiko installed - pip intstall paramiko
 
 import paramiko
 
-pdu_list = ('172.xxx.xxx.xxx','172.xxx.xxx.xxx','172.xxx.xxx.xxx')
+remote_ip_list = ('xxx.xxx.xxx.xxx','xxx.xxx.xxx.xxx','xxx.xxx.xxx.xxx')
 
 def connect_and_run_ssh(remoteip):
     # Connect
@@ -12,12 +13,15 @@ def connect_and_run_ssh(remoteip):
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     client.connect(remoteip, username='<user>', password='*******',look_for_keys=False, allow_agent=False)
 
-    # Run a command (execute PHP interpreter)
+    # Run a command (execute command to get temperature readings)
     stdin, stdout, stderr = client.exec_command('tempreading c')
+
+    # Print output based on your requirements
     # print(type(stdin))  # <class 'paramiko.channel.ChannelStdinFile'>
     # print(type(stdout))  # <class 'paramiko.channel.ChannelFile'>
     # print(type(stderr))  # <class 'paramiko.channel.ChannelStderrFile'>
 
+    #I used decode UTF-8. you can use decoding options. 
     outinfo = stdout.read().decode("utf8")
     outinfo = stdout.read().decode("utf8")
     outinfo = stdout.read().decode("utf8")
@@ -31,7 +35,7 @@ def connect_and_run_ssh(remoteip):
 
     return outinfo
 
-for remoteip in pdu_list:
+for remoteip in remote_ip_list:
     print (f'\nConnecting to: {remoteip}\n')
     dataout = connect_and_run_ssh(remoteip)
     print (dataout)
